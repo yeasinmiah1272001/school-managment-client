2197307f6cf1ea8ab7fd80236ff5c4cce15f550d
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-03-18 17:49:20 +0600

    solve

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./routes/Router.jsx";
